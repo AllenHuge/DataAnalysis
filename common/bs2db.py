c758e93d1151fc84f7ac7ccfe97ac5b4d974223c
zxhu <zhixue_hu@126.com>
2020-02-26 14:09:43 +0800

    (+)从上交所官网爬取最新上证A股证券代码，并baostock获取上证A股证券基本信息

---
+++ common/bs2db.py
@@ -0,0 +1,88 @@
+import baostock as bstk
+import pandas as pd
+import numpy as np
+from common.db_operation import df2mysql
+import bs4 as bs
+import requests #python的http客户端
+import re
+import sqlalchemy
+
+
+def getHtml(page):
+    start = (page - 1)*25
+    end = page*25
+    url = '''
+    http://yunhq.sse.com.cn:32041//v1/sh1/list/exchange/equity?callback=jQuery1124023881100803018573_1582514431484&select=code%2Cname%2Copen%2Chigh%2Clow%2Clast%2Cprev_close%2Cchg_rate%2Cvolume%2Camount%2Ctradephase%2Cchange%2Camp_rate%2Ccpxxsubtype%2Ccpxxprodusta&order=&begin='''\
+          +str(start)\
+          +'''&end='''\
+          +str(end)+'''&_=1582514431493
+    '''
+    r = requests.get(url)
+    pat = '''"list":\[(.*?)\]}'''
+    data = re.compile(pat,re.S).findall(r.text)
+    return data
+
+
+def getOnePageStock(page):
+    html = getHtml(page)
+    pat = '''\[(.*?)\]'''
+    data = re.compile(pat, re.S).findall(html[0])
+    return [x.replace('''"''', "").split(',')[0] for x in data]
+
+
+def getCode():
+    page = 1
+    stocks = getOnePageStock(page)
+    # 自动爬取多页，并在结束时停止
+    while True:
+        page += 1
+        if getHtml(page) != getHtml(page - 1):
+            stocks.extend(getOnePageStock(page))
+        else:
+            break
+    return stocks
+
+
+def getStockInfo():
+    # 登陆系统
+    lg = bstk.login()
+    # 显示登陆返回信息
+    print('login respond  error_msg:'+lg.error_msg)
+    # 获取证券基本资料
+    data_list = []
+    stock_list = getCode()
+    print(stock_list)
+    for stock in stock_list:
+        stock_code = stock
+        rs = bstk.query_stock_basic(code="sh.{}".format(stock_code))
+        # rs = bs.query_stock_basic(code_name="浦发银行")  # 支持模糊查询
+
+        # 打印结果集
+        while (rs.error_code == '0') & rs.next():
+            # 获取一条记录，将记录合并在一起
+            data_list.append(rs.get_row_data())
+    result = pd.DataFrame(data_list, columns=rs.fields)
+    # 登出系统
+    bstk.logout()
+    df = result
+    df.columns = ['stock_code','stock_name','ipo_date','out_date','stock_type','stock_status']
+    df['out_date'] = result['out_date'].apply(lambda x: np.NaN if x=='' else x)
+    return df
+
+
+if __name__ == '__main__':
+    df = getStockInfo()
+    #股票基本信息插入mysql数据库
+    df2mysql(df=df
+             ,schema_name='ext_data_stock'
+             ,table_name='stock_base_info'
+             ,if_exists='replace'
+             ,dtype={
+                    "stock_code":sqlalchemy.types.NVARCHAR(length=10),
+                    "stock_name":sqlalchemy.types.NVARCHAR(length=10),
+                    "ipo_date":sqlalchemy.types.DateTime(),
+                    "out_date":sqlalchemy.types.DateTime(),
+                    "stock_type":sqlalchemy.types.NVARCHAR(length=5),
+                    "stock_status":sqlalchemy.types.NVARCHAR(length=5),
+                }
+             )
